6d37abd6bc78464fab44e2e2d82c8bde650ecd55
tibor75 <64092150+tibor75@users.noreply.github.com>
2020-04-27 17:48:36 +0200

    insert first part

---
+++ scripts.js
@@ -0,0 +1,184 @@
+let name = 'Adam';
+
+console.log(name + "'s apples")
+console.log('Hello World!');
+
+// This is a JavaScript comment
+
+/*
+This is a multiline
+JavaScript comment.
+*/
+
+// This is the first part:
+
+/* 12+3
+15
+12-5;
+7
+12*4
+48
+12/3
+4
+12/4*8-15
+9 
+let age
+undefined
+age = 33
+33
+12/4
+3
+age / 3
+11
+age = 12
+12
+age / 4
+3
+let legs = 4
+undefined
+legs * 8
+32
+let legs = 2;
+undefined
+legs = 2;
+2
+legs = 2
+2
+let name = Adam
+VM664:1 Uncaught ReferenceError: Adam is not defined
+    at <anonymous>:1:12
+(anonymous) @ VM664:1
+let anotherAge = age
+undefined
+anotherAge
+12
+let name = 'Adam'
+undefined
+name
+"Adam"
+name = "Peti"
+"Peti"
+name
+"Adam's apples"
+"Adam's apples"
+let name = 'something'
+undefined
+name
+"something"
+true
+true
+false
+false
+let oldEnoughToDrive = false
+undefined
+oldEnoughToDrive = true
+true
+age + 5
+17
+name + 'else'
+"somethingelse"
+name + 'else' + name + age
+"somethingelsesomething12"
+name + 'else' + 14 + name + age
+"somethingelse14something12"
+'adam' - 'apple'
+NaN
+true && true
+true
+false && true
+false
+false && false
+false
+true && true
+true
+true || true
+true
+false || true
+true
+false || false
+false
+true || false
+true
+"true && (false || (true && false))"
+"true && (false || (true && false))"
+true && (false || (true && false))
+false
+!true
+false
+!false
+true
+oldEnoughToDrive
+true
+!oldEnoughToDrive
+false
+let color1 = 'orange'
+undefined
+let color2 = 'blue'
+undefined
+let colors = ['orange', 'blue', 'green', 'purple']
+undefined
+colors
+(4) ["orange", "blue", "green", "purple"]
+colors [0]
+"orange"
+colors [1]
+"blue"
+colors [3]
+"purple"
+colors [2]
+"green"
+colors.length
+4
+colors.push('yellow')
+5
+colors
+(5) ["orange", "blue", "green", "purple", "yellow"]
+[12, 54, 1212]
+(3) [12, 54, 1212]
+[treu, false, true]
+[true, false, true]
+(3) [true, false, true]
+[colors, colors, colors]
+(3) [Array(5), Array(5), Array(5)]
+[12, 'hello', true]
+(3) [12, "hello", true]
+colors
+(5) ["orange", "blue", "green", "purple", "yellow"]
+colors[2] = 'lime'
+"lime"
+colors
+(5) ["orange", "blue", "lime", "purple", "yellow"]
+[33, 'Adam', true]
+(3) [33, "Adam", true]
+let person = {}
+undefined
+let person = {
+name: 'Adam',
+age: 33,
+oldEnoughToDrive: true 
+}
+undefined
+prson
+person
+{name: "Adam", age: 33, oldEnoughToDrive: true}
+person.name
+"Adam"
+person.name + 10
+"Adam10"
+person.age + 10
+43
+person.name = 'Peti'
+"Peti"
+person
+{name: "Peti", age: 33, oldEnoughToDrive: true}
+name
+"something"
+age
+12
+oldEnoughToDrive
+true
+person.favoriteColor = ['lime', 'green']
+(2) ["lime", "green"]
+person
+{name: "Peti", age: 33, oldEnoughToDrive: true, favoriteColor: Array(2)}
+*/

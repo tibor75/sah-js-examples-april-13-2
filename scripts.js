fe8a1f6b81d8d3236f8a39c2619ba1c9621a154a
tibor75 <64092150+tibor75@users.noreply.github.com>
2020-04-27 18:57:12 +0200

    update second part

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ let name = 'Adam';
 console.log(name + "'s apples")
 console.log('Hello World!');
 
+$('h1').text('We are having a 10 minute break until 17:05')
+
 // This is a JavaScript comment
 
 /*
